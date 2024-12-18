98cf033b777eaa934cb6db74b9772daf5641ab8b
Erkang Xia <erkangxia@Erkangs-MBP-16.hsd1.ma.comcast.net>
2024-12-17 22:10:45 -0500

    first commit

---
+++ src/components/TestArea/Attention/Bulb.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Bulb = ({ changed, numberOfBulbs }) => {
+    const bulbs = [];
+
+    for (let i = 1; i <= numberOfBulbs; i++) {
+        bulbs.push(
+            <div key={i} className="relative my-6">
+                <div
+                    className={`absolute top-1 left-1 w-8 h-8 rounded-full ${changed >= i ? 'bg-primaryGreen opacity-50' : 'bg-primaryBlueMedium'}`}></div>
+                <div
+                    className={`absolute w-10 h-10 blur opacity-50 rounded-full ${changed >= i ? 'bg-primaryGreen' : 'hidden'}`}></div>
+            </div>
+        );
+    }
+
+    return (
+        <div className="flex flex-wrap justify-center space-x-20">
+            {bulbs}
+        </div>
+    );
+};
+
+export default Bulb;
